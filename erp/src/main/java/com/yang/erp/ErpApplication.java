f003eb083b8c1d787483171bd60883f38a4b04c7
caoshenyang <2417254000@qq.com>
2020-10-27 16:57:09 +0800

    整合swagger2

--- erp/src/main/java/com/yang/erp/ErpApplication.java
+++ erp/src/main/java/com/yang/erp/ErpApplication.java
@@ -3,11 +3,14 @@ package com.yang.erp;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * 启动类
  * @author caoshenyang
  */
+@RequestMapping
 @SpringBootApplication
 @MapperScan(basePackages = "com.yang.erp")
 public class ErpApplication {
@@ -16,4 +19,8 @@ public class ErpApplication {
         SpringApplication.run(ErpApplication.class, args);
     }
 
+    @GetMapping("/")
+    public String index(){
+        return "redirect:http://localhost:8080/erp/swagger-ui.html";
+    }
 }
