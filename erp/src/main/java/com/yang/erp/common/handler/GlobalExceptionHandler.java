3c51f5fb7caa92e8535ee122610d3ba9d116194c
caoshenyang <2417254000@qq.com>
2020-10-30 14:46:46 +0800

    配置日志

--- erp/src/main/java/com/yang/erp/common/handler/GlobalExceptionHandler.java
+++ erp/src/main/java/com/yang/erp/common/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.yang.erp.common.handler;
 
 import com.yang.erp.common.exception.ErpException;
 import com.yang.erp.common.utils.R;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,10 +13,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @author caoshenyang
  */
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public R error(Exception e) {
+        log.error(e.getMessage());
         e.printStackTrace();
         return R.error();
 
